b41d67895f5ca9cde22ab06e4d596ac5febc4cff
Goldie <goldiejackson25@yahoo.com>
2021-09-26 08:34:21 -0500

    working css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 //Link to codefortulsa api that only includes park data for parks with restrooms, drinking fountains, and playgrounds
 //API is set to 20 rows, but only has 12 parks that meet the criteria
 const BASE_URL = "https://codefortulsa.opendatasoft.com/api/records/1.0/search/?dataset=parks-and-recreation-areas&q=&rows=20&facet=operdays&facet=operhours&facet=restroom&facet=adacomply&facet=camping&facet=swimming&facet=swimmingjr&facet=waterplayg&facet=splashpad&facet=hiking&facet=fishing&facet=picnic&facet=boating&facet=hunting&facet=roadcycle&facet=mtbcycle&facet=playground&facet=golf&facet=ski&facet=soccer&facet=baseball&facet=basketball&facet=basketba_1&facet=tennis&facet=tennislit&facet=dogpark&facet=discgolf&facet=drinkingfo&facet=outdoorfit&facet=reccenter&refine.playground=Yes&refine.restroom=Yes&refine.drinkingfo=Yes";
+const LOCAL_DB_URL = "http://localhost:3000/comments/";
 
 const parkCardsContainer = document.querySelector('#middle-container');
 const deleteParkNote = document.getElementsByClassName('delete-note-button');
@@ -10,7 +11,10 @@ function init(){
     //returned an HTML collection - used spread operator saved to a variable
     const buttons = [...document.getElementsByClassName('feature-button')];
 
-    //###########################################goldie#############################FETCH DB.JSON
+    //########################################################################FETCH DB.JSON
+    fetch(LOCAL_DB_URL)
+    .then(resp => resp.json())
+    .then(json => json.forEach(function (e){renderParkNote(e["note"], e["park"])}));
 
     buttons.forEach(e => e.addEventListener('click', () => {getParks(e.id)}));
 }
@@ -44,10 +48,11 @@ function renderParkCards(parksWithFeature){
     parkCard.className = "park-card"
     const cardHeading = document.createElement('h2');
     const cardSummary = document.createElement('p');
+
     //input form to post to db.json??
     const noteForm = document.createElement('form');
-    //notesForm.setAttribute('method', 'POST');
-    //notesForm.setAttribute('action', ######)
+    //noteForm.setAttribute('method', 'POST');
+    //noteForm.setAttribute('action', LOCAL_DB_URL)
     const noteInput = document.createElement('input');
     const noteSubmit = document.createElement('input');
     
@@ -61,7 +66,7 @@ function renderParkCards(parksWithFeature){
     `
     days of operations: ${operdays.toLowerCase()}<br>
     hours open: ${operhours.toLowerCase()}<br>
-    address: ${fulladdr.toLowerCase()}<br>
+    address: ${fulladdr.toLowerCase()}<br><br>
     features: <br>
     baseball: ${baseball.toLowerCase()}<br>
     basketball: ${basketball.toLowerCase()}<br>
@@ -91,22 +96,40 @@ function renderParkCards(parksWithFeature){
 
 }
 
-function handleNoteInput(note, name){
+function handleNoteInput(note, park){
     debugger;
-    const noteInput = document.getElementById(`${name}-note-input`);
-    const parkName = name.toLowerCase();
+    const noteInput = document.getElementById(`${park}-note-input`);
+    const parkName = park.toLowerCase();
     renderParkNote(noteInput.value, parkName);
+    
+    const pojoPark = {
+        note: noteInput.value,
+        park: parkName
+    }
+    console.log(pojoPark)
+    //############################################################################################POST DB.JSON
+    
+    //either patch request instead or figure out id problem and make them independent comments
+    fetch(LOCAL_DB_URL),{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(pojoPark)
+        };
+
     note.reset();
     //WHY IS THE PAGE REFRESHING?? -- moved the event prevent default to the callback function!
 }
 
 function renderParkNote(note, park){
-    debugger;
+    //debugger;
     const bottomContainer = document.querySelector('#bottom-container');
     const parkNoteDiv = document.createElement('span');
     parkNoteDiv.id = 'park-note-div'
     const parkNote = document.createElement('p');
-    parkNote.id = 'park-note';
+    //PARKnote needs the db id to work
+    parkNote.className = 'park-note';
     const deleteParkNote = document.createElement('button');
     deleteParkNote.className = 'delete-note-button';
 
@@ -125,7 +148,8 @@ function handleDelete(e){
     debugger;
     e.target.parentNode.remove();
 
-    //delete request
+    //########################################################delete request
+
 }
 
 init();
